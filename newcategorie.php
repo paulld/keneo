342671739a76b916ed087c34ad93255e10a963e9
Paul <paul.lugagnedelpon@gmail.com>
2015-01-09 00:01:23 +0800

    Change design for client-project-mission-category relation (rela|1|2|3|4, newcategory, currcategory).

--- newcategorie.php
+++ newcategorie.php
@@ -2,26 +2,53 @@
 session_start();
 include("appel_db.php");
 
-	?>
-	<select onchange="showCurrCat(this.value)" id="w_input_90">
+?>
+<div class="col-sm-4">
+	<select onchange="showCurrCat(this.value)" class="form-control">
 		<option value="0">Cat&eacute;gorie existante</option>
 		<option value="1" selected>Ajouter une cat&eacute;gorie</option>
-	</select><br/>
-	<?php
-	echo '<table id="newimput"><tr><th id="newimputth" colspan="2">Param&egrave;tres de la nouvelle cat&eacute;gorie</th></tr>';
-	echo '<tr><td id="newimputtd">Code :</td><td id="newimputtd"><input id="w_input_90" type="text" size="15" name="newcatcode" /></td></tr>';
-	echo '<tr><td id="newimputtd">Alias :</td><td id="newimputtd"><input id="w_input_90" type="text" size="3" name="newcatplan" /></td></tr>';
-	echo '<tr><td id="newimputtd">Desc. :</td><td id="newimputtd"><input id="w_input_90" type="text" size="50" name="newcatdesc" /></td></tr>';
-	echo '<tr><td id="newimputtd">Resp. :</td><td id="newimputtd">';
-		echo '<select name="newcatresp" id="w_input_90" >';
-			echo '<option value=0></option>';
-			$affcollab = $bdd->query("SELECT * FROM rob_user WHERE actif='1' ORDER BY nom");
-			while ($optioncoll = $affcollab->fetch())
-			{
-				echo '<option value='.$optioncoll['ID'].'>'.substr ($optioncoll['prenom'],0,1).'. '.$optioncoll['nom'].'</option>';
-			}
-			$affcollab->closeCursor();
-		echo '</select></td></tr></table>';
-	echo '<input id="w_input_90val" type="submit" Value="Ajouter" />';
+	</select>
+</div>
 
-?> 
+<div class="col-sm-6">
+	<!-- <p>Param&egrave;tres de la nouvelle cat&eacute;gorie</p> -->
+	<div class="form-group form-group-new-project">
+		<label for="modcode" class="col-xs-4 control-label">Code :</label>
+		<div class="col-sm-8">
+			<input class="form-control" type="text" size="15" name="newcatcode" />
+		</div>
+	</div>
+
+	<div class="form-group form-group-new-project">
+		<label for="modcode" class="col-xs-4 control-label">Alias :</label>
+		<div class="col-sm-8">
+			<input class="form-control" type="text" size="3" name="newcatplan" />
+		</div>
+	</div>
+
+	<div class="form-group form-group-new-project">
+		<label for="modcode" class="col-xs-4 control-label">Desc. :</label>
+		<div class="col-sm-8">
+			<input class="form-control" type="text" size="50" name="newcatdesc" />
+		</div>
+	</div>
+
+	<div class="form-group form-group-new-project">
+		<label for="modcode" class="col-xs-4 control-label">Resp. :</label>
+		<div class="col-sm-8">
+
+			<select name="newcatresp" class="form-control" >
+				<option value=0></option>
+				<?php
+					$affcollab = $bdd->query("SELECT * FROM rob_user WHERE actif='1' ORDER BY nom");
+					while ($optioncoll = $affcollab->fetch()) {
+						echo '<option value='.$optioncoll['ID'].'>'.substr ($optioncoll['prenom'],0,1).'. '.$optioncoll['nom'].'</option>';
+					}
+					$affcollab->closeCursor();
+				?> 
+			</select>
+		</div>
+	</div>
+	
+</div>
+<!-- <input class="btn btn-primary" type="submit" Value="Ajouter" /> -->
