30a0ce8246b27ff17e22192788f14dce59a95ba4
Paul <paul.lugagnedelpon@gmail.com>
2015-01-10 13:40:47 +0800

    Redisign DB Mgmt/Team and modif_collab.

--- modif_collab.php
+++ modif_collab.php
@@ -18,9 +18,11 @@ include("appel_db.php");
 						
 						<section id="param-contacts">
 							<?php
-							if (isset($_POST['IDmodif']))
-							{
-								$req = "SELECT T1.ID, T2.nom, T2.prenom, T2.matricule, T6.matricule, T7.code, T3.menu, T8.tms, T4.exp, T5.jrl, T9.telephone, T9.mobile, T9.mail, T9.pays, T9.ville, T9.CP, T9.adresse, T10.recup, T10.cp, T10.rtt, T1.extstd
+							if (isset($_POST['IDmodif'])) {
+								$req = "SELECT T1.ID id, T2.nom nom, T2.prenom premom, T2.matricule matricule, 
+															 T6.matricule, T7.code, T3.menu, T8.tms, T4.exp, T5.jrl, 
+															 T9.telephone, T9.mobile, T9.mail, T9.pays, T9.ville, T9.CP, T9.adresse, 
+															 T10.recup, T10.cp, T10.rtt, T1.extstd
 										FROM rob_user_rights T1 
 										INNER JOIN rob_user T2 ON T1.ID = T2.ID
 										INNER JOIN rob_level T3 ON T1.id_lev_menu = T3.ID
@@ -34,119 +36,144 @@ include("appel_db.php");
 										WHERE T1.ID = ".$_POST['IDmodif'];
 								$reponse = $bdd->query($req );
 								$checkrep = $reponse->rowCount();
-								if ($checkrep != 0)
-								{
+								if ($checkrep != 0) {
+									while ($cur_nom = $reponse->fetch()) {
 									?>
 									<form action="collaborateurs.php" method="post">
-									<?php
-									while ($cur_nom = $reponse->fetch())
-									{
-										echo '<fieldset id="infos-complementaires">';
-										echo '<h2>Informations g&eacute;n&eacute;rale</h2>';
-										echo '<p>Nom :</p><input class="form-control form-control-small" type="text" name="nom" value="'.$cur_nom[1].'" />';
-										echo '<p>Pr&eacute;nom :</p><input class="form-control form-control-small" type="text" name="prenom" value="'.$cur_nom[2].'" />';
-										echo '<p>Trigramme :</p><input class="form-control form-control-small" type="text" size="5" name="trig" value="'.$cur_nom[3].'" />';
-										echo '</fieldset>';
-										echo '<h2>Param&egrave;tres</h2>';
-										echo '<p>Effectif de type :<select class="form-control form-control-small" name="extstd">';
+
+										<fieldset id="infos-generales" class="fieldset-align-middle">
+											<h2>Informations g&eacute;n&eacute;rale</h2>
+											<?php
+											echo '<p><span>Nom :</span><input class="form-control form-control-small" type="text" name="nom" value="'.$cur_nom[1].'" /></p>';
+											echo '<p><span>Pr&eacute;nom :</span><input class="form-control form-control-small" type="text" name="prenom" value="'.$cur_nom[2].'" /></p>';
+											echo '<p><span>Trigramme :</span><input class="form-control form-control-small" type="text" size="5" name="trig" value="'.$cur_nom[3].'" /></p>';
+											?>
+										</fieldset>
+
+										<fieldset id="parametres" class="fieldset-align-middle">
+											<h2>Param&egrave;tres</h2>
+											<?php
+											echo '<p><span>Effectif de type :</span><select class="form-control form-control-small" name="extstd">';
 											echo '<option value=1>Externe</option>';
-											if ($cur_nom[20] == 2) { echo '<option value=2 selected>Interne</option>'; }
-												else { echo '<option value=2>Interne</option>'; }
+											if ($cur_nom[20] == 2) { 
+												echo '<option value=2 selected>Interne</option>'; 
+											} else { 
+												echo '<option value=2>Interne</option>'; 
+											}
 											echo '</select></p>';
-										echo '<p>Responsable hi&eacute;rarchique :<select class="form-control form-control-small" name="resp">';
+											echo '<p><span>Responsable hi&eacute;rarchique :</span><select class="form-control form-control-small" name="resp">';
 											$reponse2 = $bdd->query("SELECT matricule, ID FROM rob_user ORDER BY matricule");
-											while ($donnee2 = $reponse2->fetch() )
-											{
-												if ($donnee2['matricule'] == $cur_nom[4]) { echo '<option value="'.$donnee2['ID'].'" selected>'.$donnee2['matricule'].'</option>'; }
-												else { echo '<option value="'.$donnee2['ID'].'">'.$donnee2['matricule'].'</option>'; }
+											while ($donnee2 = $reponse2->fetch() ) {
+												if ($donnee2['matricule'] == $cur_nom[4]) { 
+													echo '<option value="'.$donnee2['ID'].'" selected>'.$donnee2['matricule'].'</option>'; 
+												} else { 
+													echo '<option value="'.$donnee2['ID'].'">'.$donnee2['matricule'].'</option>'; 
+												}
 											}
 											$reponse2->closeCursor();
 											echo '</select></p>';
-										echo '<p>Affect&eacute; au p&ocirc;le :<select class="form-control form-control-small" name="pole">';
+											echo '<p><span>Affect&eacute; au p&ocirc;le :</span><select class="form-control form-control-small" name="pole">';
 											$reponse2 = $bdd->query("SELECT code, ID FROM rob_pole WHERE actif = 1");
-											while ($donnee2 = $reponse2->fetch() )
-											{
-												if ($donnee2['code'] == $cur_nom[5]) { echo '<option value="'.$donnee2['ID'].'" selected>'.$donnee2['code'].'</option>'; }
-												else { echo '<option value="'.$donnee2['ID'].'">'.$donnee2['code'].'</option>'; }
+											while ($donnee2 = $reponse2->fetch() ) {
+												if ($donnee2['code'] == $cur_nom[5]) { 
+													echo '<option value="'.$donnee2['ID'].'" selected>'.$donnee2['code'].'</option>'; 
+												} else { 
+													echo '<option value="'.$donnee2['ID'].'">'.$donnee2['code'].'</option>'; 
+												}
 											}
 											$reponse2->closeCursor();
 											echo '</select></p>';
-										echo '<p>Acc&egrave;s aux menus :<select class="form-control form-control-small" name="user_lev">';
+											echo '<p><span>Acc&egrave;s aux menus :</span><select class="form-control form-control-small" name="user_lev">';
 											$req = "SELECT menu, ID FROM rob_level WHERE actif = 1 AND menu <> ''";
 											$reponse2 = $bdd->query($req);
-											while ($donnee2 = $reponse2->fetch() )
-											{
-												if ($donnee2['menu'] == $cur_nom[6]) { echo '<option value="'.$donnee2['ID'].'" selected>'.$donnee2['menu'].'</option>'; }
-												else { echo '<option value="'.$donnee2['ID'].'">'.$donnee2['menu'].'</option>'; }
+											while ($donnee2 = $reponse2->fetch() ) {
+												if ($donnee2['menu'] == $cur_nom[6]) { 
+													echo '<option value="'.$donnee2['ID'].'" selected>'.$donnee2['menu'].'</option>'; 
+												} else { 
+													echo '<option value="'.$donnee2['ID'].'">'.$donnee2['menu'].'</option>'; 
+												}
 											}
 											$reponse2->closeCursor();
 											echo '</select></p>';
-										echo '<p>Droits sur la timesheet :<select class="form-control form-control-small" name="tms_lev">';
+											echo '<p><span>Droits sur la timesheet :</span><select class="form-control form-control-small" name="tms_lev">';
 											$req = "SELECT tms, ID FROM rob_level WHERE actif = 1 AND tms <> ''";
 											$reponse2 = $bdd->query($req);
-											while ($donnee2 = $reponse2->fetch() )
-											{
-												if ($donnee2['tms'] == $cur_nom[7]) { echo '<option value="'.$donnee2['ID'].'" selected>'.$donnee2['tms'].'</option>'; }
-												else { echo '<option value="'.$donnee2['ID'].'">'.$donnee2['tms'].'</option>'; }
+											while ($donnee2 = $reponse2->fetch() ) {
+												if ($donnee2['tms'] == $cur_nom[7]) { 
+													echo '<option value="'.$donnee2['ID'].'" selected>'.$donnee2['tms'].'</option>'; 
+												} else { 
+													echo '<option value="'.$donnee2['ID'].'">'.$donnee2['tms'].'</option>'; 
+												}
 											}
 											$reponse2->closeCursor();
 											echo '</select></p>';
-										echo '<p>Droits sur les frais :<select class="form-control form-control-small" name="exp_lev">';
+											echo '<p><span>Droits sur les frais :</span><select class="form-control form-control-small" name="exp_lev">';
 											$req = "SELECT exp, ID FROM rob_level WHERE actif = 1 AND exp <> ''";
 											$reponse2 = $bdd->query($req);
-											while ($donnee2 = $reponse2->fetch() )
-											{
-												if ($donnee2['exp'] == $cur_nom[8]) { echo '<option value="'.$donnee2['ID'].'" selected>'.$donnee2['exp'].'</option>'; }
-												else { echo '<option value="'.$donnee2['ID'].'">'.$donnee2['exp'].'</option>'; }
+											while ($donnee2 = $reponse2->fetch() ) {
+												if ($donnee2['exp'] == $cur_nom[8]) { 
+													echo '<option value="'.$donnee2['ID'].'" selected>'.$donnee2['exp'].'</option>'; 
+												} else { 
+													echo '<option value="'.$donnee2['ID'].'">'.$donnee2['exp'].'</option>'; 
+												}
 											}
 											$reponse2->closeCursor();
 											echo '</select></p>';
-										echo '<p>Droits sur les journaux : <select class="form-control form-control-small" name="jrl_lev">';
+											echo '<p><span>Droits sur les journaux :</span><select class="form-control form-control-small" name="jrl_lev">';
 											$req = "SELECT jrl, ID FROM rob_level WHERE actif = 1 AND jrl <> ''";
 											$reponse2 = $bdd->query($req);
-											while ($donnee2 = $reponse2->fetch() )
-											{
-												if ($donnee2['jrl'] == $cur_nom[9]) { echo '<option value="'.$donnee2['ID'].'" selected>'.$donnee2['jrl'].'</option>'; }
-												else { echo '<option value="'.$donnee2['ID'].'">'.$donnee2['jrl'].'</option>'; }
+											while ($donnee2 = $reponse2->fetch() ) {
+												if ($donnee2['jrl'] == $cur_nom[9]) { 
+													echo '<option value="'.$donnee2['ID'].'" selected>'.$donnee2['jrl'].'</option>'; 
+												} else { 
+													echo '<option value="'.$donnee2['ID'].'">'.$donnee2['jrl'].'</option>'; 
+												}
 											}
 											$reponse2->closeCursor();
 											echo '</select></p>';
-										echo '<fieldset id="infos-complementaires">';
-										echo '<h2>Coordonn&eacute;es</h2>';
-										echo '<p>T&eacute;l&eacute;phone :</p><input class="form-control form-control-small" type="text" size="20" name="tel" value="'.$cur_nom[10].'" />';
-										echo '<p>Mobile :</p><input class="form-control form-control-small" type="text" size="20" name="mobile" value="'.$cur_nom[11].'" />';
-										echo '<p>Email :</p><input class="form-control form-control-small" type="text" size="50" name="mail" value="'.$cur_nom[12].'" />';
-										echo '<p>Adresse :</p><input class="form-control form-control-small" type="text" size="50" name="adresse" value="'.$cur_nom[16].'" />';
-										echo '<p>CP :</p><input class="form-control form-control-small" type="text" size="7" name="cp" value="'.$cur_nom[15].'" />';
-										echo '<p>Ville :</p><input class="form-control form-control-small" type="text" size="20" name="ville" value="'.$cur_nom[14].'" />';
-										echo '<p>Pays :</p><input class="form-control form-control-small" type="text" size="15" name="pays" value="'.$cur_nom[13].'" />';
-										echo '</fieldset>';
-										echo '<section id="param-conges">';
-										echo '<h2>Solde des cong&eacute;s, RTT et r&eacute;cup&eacute;rations</h2>';
+											?>
+										</fieldset>
+
+										<fieldset id="coordonnees" class="fieldset-align-middle">
+											<h2>Coordonn&eacute;es</h2>
+											<?php
+											echo '<p><span>T&eacute;l&eacute;phone :</span><input class="form-control form-control-small" type="text" size="20" name="tel" value="'.$cur_nom[10].'" /></p>';
+											echo '<p><span>Mobile :</span><input class="form-control form-control-small" type="text" size="20" name="mobile" value="'.$cur_nom[11].'" /></p>';
+											echo '<p><span>Email :</span><input class="form-control form-control-small" type="text" size="50" name="mail" value="'.$cur_nom[12].'" /></p>';
+											echo '<p><span>Adresse :</span><input class="form-control form-control-small" type="text" size="50" name="adresse" value="'.$cur_nom[16].'" /></p>';
+											echo '<p><span>CP :</span><input class="form-control form-control-small" type="text" size="7" name="cp" value="'.$cur_nom[15].'" /></p>';
+											echo '<p><span>Ville :</span><input class="form-control form-control-small" type="text" size="20" name="ville" value="'.$cur_nom[14].'" /></p>';
+											echo '<p><span>Pays :</span><input class="form-control form-control-small" type="text" size="15" name="pays" value="'.$cur_nom[13].'" /></p>';
+											?>
+										</fieldset>
+
+										<fieldset id="param-conges" class="fieldset-align-middle">
+											<h2>Solde des cong&eacute;s, RTT et r&eacute;cup&eacute;rations</h2>
+											<?php
 											$req = "SELECT sum(recup) FROM rob_temps 
 												WHERE userID=".$_POST['IDmodif']." AND recup <> 0 AND phaseID='1' AND recupValid IS NULL
 												GROUP BY userID";
 											$check = $bdd->query($req);
 											$checkrow=$check->rowCount();
-											if ($checkrow != 0)
-											{
+											if ($checkrow != 0) {
 												$dchk = $check->fetch();
-												echo '<p>R&eacute;cup :'.number_format($dchk[0],2,",","").'<input type="hidden" name="absrecup" value="0" /></p>';
+												echo '<p><span>R&eacute;cup :</span><span>'.number_format($dchk[0],2,",","").'</span><input type="hidden" name="absrecup" value="0" /></p>';
 											} else {
-												echo '<p>R&eacute;cup :'.number_format(0,2,",","").'<input type="hidden" name="absrecup" value="0" /></p>';
+												echo '<p><span>R&eacute;cup :</span><span>'.number_format(0,2,",","").'</span><input type="hidden" name="absrecup" value="0" /></p>';
 											}
 											$check->closeCursor();
-										echo '<p>Cong&eacute;s pay&eacute;s :<input class="form-control form-control-small" type="text" size="5" name="abscp" value="'.$cur_nom[18].'" /></p>';
-										echo '<p>RTT :<input class="form-control form-control-small" type="text" size="15" name="absrtt" value="'.$cur_nom[19].'" /></p>';
-										echo '</section>';
+											echo '<p><span>Cong&eacute;s pay&eacute;s :</span><input class="form-control form-control-small" type="text" size="5" name="abscp" value="'.$cur_nom[18].'" /></p>';
+											echo '<p><span>RTT :</span><input class="form-control form-control-small" type="text" size="15" name="absrtt" value="'.$cur_nom[19].'" /></p>';
+											?>
+										</fieldset>
+
+											<?php
 									}
 										echo '<input type="hidden" value="'.$_POST['IDmodif'].'" name="IDmodif" /><input class="btn btn-small btn-primary btn-param" type="submit" Value="Enregistrer les modifications" name="Valider" />';
 										?>
 									</form>
 									<?php
-								}
-								else
-								{
+								} else {
 									echo 'on n\'a rien';
 								}
 								$reponse->closeCursor();
